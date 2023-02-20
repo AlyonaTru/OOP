4a3442c19af30d94fd73979c14639aadd6c830f2
Alyona <mirka-sobaka@inbox.ru>
2023-02-20 11:34:36 +0300

    hw2

---
+++ Homework_2/Assignment2.java
@@ -0,0 +1,8 @@
+package Homework_2;
+
+public class Assignment2 { // Класс, содержащий основную функцию
+
+    public static void main(String[] args) { // Параметр args указывает аргументы командной строки
+        TreeGUI treeGUI = new TreeGUI();
+    }
+}
